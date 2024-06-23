0c66b116fcefd7594dca3976a7624eb3145d6ec8
Amir256456 <amir1008047@gmail.com>
2024-06-23 11:34:34 +0500

    dashboard

--- src/app/i/page.tsx
+++ src/app/i/page.tsx
@@ -4,6 +4,8 @@ import { Heading } from '@/components/ui/Heading'
 
 import { NO_INDEX_PAGE } from '@/constants/seo.constants'
 
+import { Statistics } from './Statistics'
+
 export const metadata: Metadata = {
 	title: 'Dashboard',
 	...NO_INDEX_PAGE
@@ -13,6 +15,7 @@ export default function DashboardPage() {
 	return (
 		<div>
 			<Heading title='Statistics' />
+			<Statistics />
 		</div>
 	)
 }
