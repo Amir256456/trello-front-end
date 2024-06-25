d11f5690f35cfdd98f914dbfb8420fc568c76262
Amir256456 <amir1008047@gmail.com>
2024-06-25 10:41:57 +0500

    time-block

--- src/config/pages-url.config.ts
+++ src/config/pages-url.config.ts
@@ -5,7 +5,7 @@ class DASHBOARD {
 	TASKS = `${this.root}/tasks`
 	HABITS = `${this.root}/habits`
 	TIMER = `${this.root}/timer`
-	TIME_BLOCKING = `${this.root}/time_blocking`
+	TIME_BLOCKING = `${this.root}/time-blocking`
 	SETTINGS = `${this.root}/settings`
 }
 
